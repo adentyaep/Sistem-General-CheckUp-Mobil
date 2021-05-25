06ee2b3e5d1d8684647b11dc21ff514f88fdb2d5
Adentya Elmas Pranawa <111201912304@mhs.dinus.ac.id>
2021-05-25 18:47:17 +0700

    update

--- src/ta_uas/Kendaraan.java
+++ src/ta_uas/Kendaraan.java
@@ -9,30 +9,25 @@ package ta_uas;
  *
  * @author USER
  */
-public class Kendaraan {
-    private int kode;
-    private String nama;
+public class Kendaraan extends Konsumen{
+    
+    private String namaMobil;
     private int roda;
     private String jbakar;
-    
-    
-    public Kendaraan (int kode, String nama, int roda, String jbakar){
-        setKode(kode);
-        setNama(nama);
-        setRoda(roda);
-        setJbakar(jbakar);
-    }
-    
-    public Kendaraan(){
-        
+
+    public Kendaraan(String namaMobil, int roda, String jbakar, int kode, String nama, String alamat) {
+        super(kode, nama, alamat);
+        this.namaMobil = namaMobil;
+        this.roda = roda;
+        this.jbakar = jbakar;
     }
 
-    public String getNama() {
-        return nama;
+    public String getNamaMobil() {
+        return namaMobil;
     }
 
-    public void setNama(String nama) {
-        this.nama = nama;
+    public void setNamaMobil(String namaMobil) {
+        this.namaMobil = namaMobil;
     }
 
     public int getRoda() {
@@ -50,13 +45,7 @@ public class Kendaraan {
     public void setJbakar(String jbakar) {
         this.jbakar = jbakar;
     }
-
-    public int getKode() {
-        return kode;
-    }
-
-    public void setKode(int kode) {
-        this.kode = kode;
-    }
+    
+    
     
 }
