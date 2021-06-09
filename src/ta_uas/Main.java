bd00abe2a6cfa68f0960d47902ceef1f9d35edfe
Adentya Elmas Pranawa <111201912304@mhs.dinus.ac.id>
2021-06-09 13:03:41 +0700

    update

--- src/ta_uas/Main.java
+++ src/ta_uas/Main.java
@@ -32,32 +32,32 @@ public class Main {
     
     public static void main(String[] args){
         
-        ArrayList<Kendaraan> mobil = new ArrayList<Kendaraan>();
-        mobil.add(new Kendaraan(1, "Panther", 4, "Solar"));
-        mobil.add(new Kendaraan(2, "Innova", 4, "Pertamax"));
-        mobil.add(new Kendaraan(3, "Bentley", 4, "Pertamax plus"));
-        
-        ArrayList<Konsumen> orang = new ArrayList<Konsumen>();
-        orang.add(new Konsumen(1, "Adentya", "Semarang"));
-        orang.add(new Konsumen(2, "Yosua", "Wonogiri"));
-        orang.add(new Konsumen(3, "Arya", "Jogja"));
-        
-        ArrayList<Servis> fix = new ArrayList<Servis>();
-        fix.add(new Servis("Oli", 40));
-        fix.add(new Servis("Bemper depan", 30));
-        fix.add(new Servis("Rem Hidrolik", 80));
-        
-        for (int i = 0; i < mobil.size(); i++){
-            System.out.println("-----------------------------------------");
-            System.out.println("Servis ke - "+ (i+1));
-            System.out.println("Pemilik             : "+ orang.get(i).getNama());
-            System.out.println("alamat              : "+ orang.get(i).getAlamat());
-            System.out.println("Mobil               : "+ mobil.get(i).getNama());
-            System.out.println("Roda                : "+ mobil.get(i).getRoda());
-            System.out.println("Bahan bakar         : "+ mobil.get(i).getJbakar());
-            System.out.println("Check up bagian     : "+ fix.get(i).getPart());
-            System.out.println("Tingkat kerusakan   : "+ fix.get(i).getKerusakan());
-            System.out.println("Kategori            : "+ fix.get(i).kategori(fix.get(i).getKerusakan()));
-        }
+//        ArrayList<Kendaraan> mobil = new ArrayList<Kendaraan>();
+//        mobil.add(new Kendaraan(1, "Panther", 4, "Solar"));
+//        mobil.add(new Kendaraan(2, "Innova", 4, "Pertamax"));
+//        mobil.add(new Kendaraan(3, "Bentley", 4, "Pertamax plus"));
+//        
+//        ArrayList<Konsumen> orang = new ArrayList<Konsumen>();
+//        orang.add(new Konsumen(1, "Adentya", "Semarang"));
+//        orang.add(new Konsumen(2, "Yosua", "Wonogiri"));
+//        orang.add(new Konsumen(3, "Arya", "Jogja"));
+//        
+//        ArrayList<Servis> fix = new ArrayList<Servis>();
+//        fix.add(new Servis("Oli", 40));
+//        fix.add(new Servis("Bemper depan", 30));
+//        fix.add(new Servis("Rem Hidrolik", 80));
+//        
+//        for (int i = 0; i < mobil.size(); i++){
+//            System.out.println("-----------------------------------------");
+//            System.out.println("Servis ke - "+ (i+1));
+//            System.out.println("Pemilik             : "+ orang.get(i).getNama());
+//            System.out.println("alamat              : "+ orang.get(i).getAlamat());
+//            System.out.println("Mobil               : "+ mobil.get(i).getNama());
+//            System.out.println("Roda                : "+ mobil.get(i).getRoda());
+//            System.out.println("Bahan bakar         : "+ mobil.get(i).getJbakar());
+//            System.out.println("Check up bagian     : "+ fix.get(i).getPart());
+//            System.out.println("Tingkat kerusakan   : "+ fix.get(i).getKerusakan());
+//            System.out.println("Kategori            : "+ fix.get(i).kategori(fix.get(i).getKerusakan()));
+//        }
     }
 }
