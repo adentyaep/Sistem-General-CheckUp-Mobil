767fc0348cbe5d0f1d664e4c08d15d07e69a8ef9
Adentya Elmas Pranawa <111201912304@mhs.dinus.ac.id>
2021-06-16 09:40:06 +0700

    tambah tombol hapus

--- src/ta_uas/Form_checkup.java
+++ src/ta_uas/Form_checkup.java
@@ -88,6 +88,7 @@ public class Form_checkup extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         tb_data = new javax.swing.JTable();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -198,6 +199,14 @@ public class Form_checkup extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(tb_data);
 
+        jButton2.setBackground(new java.awt.Color(255, 51, 51));
+        jButton2.setText("HAPUS DATA");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -230,7 +239,10 @@ public class Form_checkup extends javax.swing.JFrame {
                                     .addComponent(jLabel11)
                                     .addComponent(jLabel12)
                                     .addComponent(jLabel13)
-                                    .addComponent(jButton1))
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(jButton1)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(jButton2)))
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addGroup(jPanel1Layout.createSequentialGroup()
                                         .addGap(77, 77, 77)
@@ -261,7 +273,7 @@ public class Form_checkup extends javax.swing.JFrame {
                                             .addComponent(rbLampuMati)
                                             .addComponent(rbWiperTidakBerfungsi)
                                             .addComponent(rbTidakLengkap))))))
-                        .addGap(0, 461, Short.MAX_VALUE)))
+                        .addGap(0, 449, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -338,7 +350,9 @@ public class Form_checkup extends javax.swing.JFrame {
                     .addComponent(rbLengkap)
                     .addComponent(rbTidakLengkap))
                 .addGap(22, 22, 22)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 199, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(261, Short.MAX_VALUE))
@@ -444,6 +458,21 @@ public class Form_checkup extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+         DefaultTableModel model = (DefaultTableModel) tb_data.getModel();
+        if(tb_data.getSelectedRow()==-1){
+            if(tb_data.getRowCount()==0){
+                JOptionPane.showMessageDialog(null, "Tidak ada data dalam tabel");
+            }else{
+                JOptionPane.showMessageDialog(null, "Silahkan Pilih Data Yang Akan dihapus Terlebih Dahulu!");
+            }
+        }else{
+            model.removeRow(tb_data.getSelectedRow());
+            JOptionPane.showMessageDialog(null, "Data Berhasil Dihapus");
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -481,6 +510,7 @@ public class Form_checkup extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
